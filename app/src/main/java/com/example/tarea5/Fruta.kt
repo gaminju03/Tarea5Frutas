70023d4b46acd954cd46c5699a181991bfc4a769
Juan Gamino <gamino_juan03@hotmail.com>
2020-04-19 18:13:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tarea5/Fruta.kt
@@ -0,0 +1,10 @@
+package com.example.tarea5
+
+class Fruta (nombre:String, Imagen:Int) {
+    var nombre:String = ""
+    var Imagen:Int = 0
+    init{
+        this.nombre = nombre
+        this.Imagen = Imagen
+    }
+}
